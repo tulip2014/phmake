93d42d3161cf0a7dcdd6f4d3b450a621782fe5e1
chrosyou <646680299@qq.com>
2016-04-14 23:13:08 +0800

    ignore

---
+++ jkmake/jktest/tag/first.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to XAMPP</title>
+</head>
+<body>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: macalyou
+ * Date: 2016/4/5
+ * Time: 22:59
+ */
+
+echo "hello php!";
+$a = 10;
+echo $a;
+
+function fab($n)
+{
+    if ($n <= 1) {
+        return 1;
+    } elseif ($n == 2) {
+        return 1;
+    } else {
+        return fab($n - 1) + fab($n - 2);
+    }
+}
+
+echo fab(10);
+echo '良好';
+
+?>
+
+</body>
+</html>
+
